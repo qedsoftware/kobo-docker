309d238e96f1566cd34a64fc5ecfc0ac876e1c20
Mateusz Urbańczyk <mu@qed.ai>
2019-03-09 15:32:21 +0100

    Wait untill services in containers are responding

--- scripts/wait_for_mongo.bash
+++ scripts/wait_for_mongo.bash
@@ -2,5 +2,5 @@
 set -e
 
 echo 'Waiting for container `mongo`.'
-dockerize -timeout=40s
+dockerize -timeout=40s -wait tcp://mongo:27017
 echo 'Container `mongo` up.'
--- scripts/wait_for_rabbit.bash
+++ scripts/wait_for_rabbit.bash
@@ -2,5 +2,5 @@
 set -e
 
 echo 'Waiting for container `rabbit`.'
-dockerize -timeout=40s
+dockerize -timeout=40s -wait tcp://rabbit:5672
 echo 'Container `rabbit` up.'
